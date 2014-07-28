27a6b39f4b84d1b316e5c2b0a745bee247ca6242
shioju <shioju12@gmail.com>
2014-07-28 15:29:51 +0800

    debugged errors_tests.js

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.on_use(function (api, where) {
 });
 
 Package.on_test(function(api) {
-  api.use('errors', 'client');
+  api.use('shioju-errors', 'client');
   api.use(['tinytest', 'test-helpers'], 'client');
   
   api.add_files('errors_tests.js', 'client');
